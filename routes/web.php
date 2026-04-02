9fecec833aef19d863150ce4bd0b2806d1e61012
youcode safi <tajat.nourelhouda@gmail.com>
2026-04-02 12:29:14 +0100

    chore(routes): define resource routes for recipes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function () {
+    Route::resource('recipes', RecipeController::class);
+    
+});
