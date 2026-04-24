24e3ff538ebef659aef0d157f7b20aab612484f7
youcode safi <tajat.nourelhouda@gmail.com>
2026-04-24 09:53:33 +0100

    feat(admin): Enregistrer le Middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,9 +10,15 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
+
